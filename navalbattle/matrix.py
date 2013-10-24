9b228aef85a4a24807fba0f4064523ee15224058
unknown <minus@.(none)>
2013-10-24 22:22:55 +0200

    Added class matrix

---
+++ navalbattle/matrix.py
@@ -0,0 +1,18 @@
+'''
+Created on 24/ott/2013
+
+@author: minus
+'''
+
+class Matrix():
+    '''
+    Simple 2d array class
+    '''
+
+
+    def __init__(self,rows,cols):
+        '''
+        Builds a new matrix
+        '''
+        self.rows=rows
+        self.cols=cols
