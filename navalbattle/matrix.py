72f8c4d789e59362c71eefc82298c991c5e2d849
unknown <minus@.(none)>
2013-10-26 23:08:08 +0200

    Initialized by string

--- navalbattle/matrix.py
+++ navalbattle/matrix.py
@@ -10,13 +10,38 @@ class Matrix():
     '''
 
 
-    def __init__(self,rows,cols):
+    def __init__(self,rows=None,cols=None,init_string=None):
         '''
         Builds a new matrix
         '''
-        self.rows=rows
-        self.cols=cols
-        self.__matrix = [[None]*cols for _ in range(rows)]
+        if rows :
+            self.rows=rows
+            self.cols=cols
+            self.__matrix = [[None]*cols for _ in range(rows)]
+        else:
+            lines = init_string.splitlines()
+            self.__matrix=[]
+            min_col = 9999
+            max_col = 0
+            for row in lines:
+                if not row.isspace() and not len(row)==0:
+                    self.__matrix.append(row)
+                    for i in range(len(row)):
+                        c=row[i]
+                        if not c.isspace():
+                            min_col=min(min_col,i)
+                            max_col=max(max_col,i)
+            for i in range(len(self.__matrix)):
+                str_row=self.__matrix[i]
+                char_array = []
+                for j in range(min_col,max_col+1):
+                    if len(str_row)>j:
+                        char_array.append(str_row[j])
+                    else:
+                        char_array.append(' ')
+                self.__matrix[i]=char_array
+            
+        
     
     def set(self,item,row,col):
         if col < self.cols and col>0:
--- test/matrixtest.py
+++ test/matrixtest.py
@@ -40,6 +40,29 @@ class Test(unittest.TestCase):
         my_array = matrix.array()
         self.assertIsNotNone(my_array)
 
+    def testInitWithString(self):
+        astr = """
+        12
+        34
+        56
+        """
+        matrix = Matrix(init_string=astr)
+        self.assertEqual(['1','2'], matrix.array()[0])
+        self.assertEqual(['3','4'], matrix.array()[1])
+        self.assertEqual(['5','6'], matrix.array()[2])
+        
+    def testInitWithString2(self):
+        astr = """
+         2
+        3
+        56
+        """
+        matrix = Matrix(init_string=astr)
+        self.assertEqual([' ','2'], matrix.array()[0])
+        self.assertEqual(['3',' '], matrix.array()[1])
+        self.assertEqual(['5','6'], matrix.array()[2])
+        
+
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testMatrixConstructor']
     unittest.main()
